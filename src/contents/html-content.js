7fc36056aac15bd8e0d8fd98681a123b735a9c16
hp-qwlt <haripriya@qwlt.io>
2025-04-14 22:29:07 +0530

    [HP] - Updated content

--- src/contents/html-content.js
+++ src/contents/html-content.js
@@ -51,7 +51,7 @@ export const bi_weekly_notes_14_15_2025 = `
         <p>The past couple of weeks have been a mix of movement and pause—some travel, slow catch-ups, lots of learning, and a few changes along the way.</p>
         <hr/>
         <ul>
-            <li>Been trying out a few AI tools. I also learned about MCP <a href="https://youtu.be/7j_NE6Pjv-E?si=nBdQo9apsPH92xHl">(Model Context Protocol)</a> and found a great explanation that finally clicked. It's exciting how MCP can standardise context sharing and reduce LLM hallucinations.</li>
+            <li>Been trying out a few AI tools. I also learned about MCP <a href="https://youtu.be/7j_NE6Pjv-E?si=nBdQo9apsPH92xHl">(Model Context Protocol)</a>. It's exciting how it standardises context sharing and reduce LLM hallucinations. This is an exciting time for tech enthusiasts.</li>
             <li>I added a <b>subscription</b> service to my blog—top right corner of my <a href="https://blog.haripriya.org">homepage</a>. A friend asked for it, so I waited until it felt useful. If you subscribe now, you'll start getting newsletters from my next post onward.
                 <ul>
                     <li>I moved my email backend to <a href="https://resend.com/">Resend</a>. Integration was smooth and the service feels really light. No unnecessary bloat, just works.</li>
